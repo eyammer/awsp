d229d11367101b5eb16d99e6789e8b6ef6fbe793
Eytan Yammer <eyammer@crossriver.com>
2022-09-30 18:02:51 +0300

    working app

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/manifoldco/promptui"
+	"gopkg.in/ini.v1"
 	"os"
 	"strings"
 )
 
-func precheck() {
-	homeDir, _ := os.UserHomeDir()
-	awsConfigPath := fmt.Sprintf("%s/.aws/config", homeDir)
+func precheck(awsConfigPath string) {
+
 	_, err := os.Stat(awsConfigPath)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -26,10 +27,64 @@ func precheck() {
 	}
 }
 
-func getAwsConfig() {
+func getAwsConfig(awsConfigPath string) []string {
+	config, err := ini.Load(awsConfigPath)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Error loading cinfig file: %s", awsConfigPath))
+		os.Exit(-1)
+	}
+	sections := config.SectionStrings()
+	return sections
+}
 
+func selectProfile(profiles []string) string {
+	prompt := promptui.Select{
+		Label: "Select Profile",
+		Items: profiles,
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		os.Exit(-1)
+	}
+
+	fmt.Printf("You choose %q\n", result)
+	return result
+}
+
+func setDefaultProfile(defaultProfile string, awsConfigPath string) {
+	config, err := ini.Load(awsConfigPath)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Error loading cinfig file: %s", awsConfigPath))
+		os.Exit(-1)
+	}
+	if config.HasSection("default") {
+		if !config.Section("default").HasKey("awspDefault") {
+			for _, element := range config.Section("default").KeyStrings() {
+				config.Section("old_modified_default").Key(element).SetValue(config.Section("default").Key(element).String())
+			}
+		}
+	}
+	config.DeleteSection("default")
+	config.NewSection("default")
+	for _, element := range config.Section(defaultProfile).KeyStrings() {
+		config.Section("default").Key(element).SetValue(config.Section(defaultProfile).Key(element).String())
+	}
+	config.Section("default").Key("awspDefault").SetValue("true")
+	//config.Section("default").SetBody(config.Section(defaultProfile))
+	config.SaveTo(awsConfigPath)
+	//	todo - add awspDefault to an element in the default section as well as in the section selected
 }
 func main() {
-	precheck()
+	homeDir, _ := os.UserHomeDir()
+	awsConfigPath := fmt.Sprintf("%s/.aws/config", homeDir)
+	awsCredsPath := fmt.Sprintf("%s/.aws/credentials", homeDir)
 
+	precheck(awsConfigPath)
+	profiles := getAwsConfig(awsConfigPath)
+	selectedProfile := selectProfile(profiles)
+	setDefaultProfile(selectedProfile, awsConfigPath)
+	setDefaultProfile(selectedProfile, awsCredsPath)
 }
